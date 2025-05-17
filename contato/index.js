b0a677fce84ebde69692bd48c1a26d14acd75eb3
Matheus Oliveira <matheus.silvagab@gmail.com>
2025-05-16 21:46:53 -0300

    final upload

---
+++ contato/index.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('formulario')
+
+form.addEventListener('submit', ( e ) => {
+
+    e.preventDefault();
+    alert('Dados enviados com sucesso!')
+
+    form.reset();
+
+})
